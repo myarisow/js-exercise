1facb9e2e9c343dbe328035a2e9acc8fd3fb6b4b
myarisow <mohamedyarisow22847@gmail.com>
2026-09-21 23:04:16 +0300

    submit exercise 2

---
+++ exercise 2/main.js
@@ -0,0 +1,19 @@
+let a = 15;
+let b = "15";
+
+
+console.log(a == b); 
+
+console.log(a === b);
+
+console.log(a != b);  
+
+console.log(a !== b);
+
+console.log(a > b);   
+
+console.log(a < b);   
+
+console.log(a >= b);  
+
+console.log(a <= b); 
