d096bed8b2c8341b0b5a238ecb55bbae2cb0c639
myarisow <mohamedyarisow22847@gmail.com>
2026-09-24 07:59:23 +0300

    submit exercise 4

---
+++ exerice 4/main.js
@@ -0,0 +1,28 @@
+// //funtion
+
+function add(a, b){
+
+   console.log(a + b);
+
+}
+
+add(50, 89);
+add(40, 35);
+add(20, 45);
+add(97, 56);
+add(100, 2);
+
+
+//function Expression
+
+const addTotal = function(a, b){
+
+    console.log(a + b);
+
+}
+
+addTotal(5, 9);
+addTotal(40, 98);
+addTotal(55, 33);
+addTotal(35, 76);
+addTotal(56,64);
