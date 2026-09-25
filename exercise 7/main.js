cc40d27a831028f09ffa0eb17a2801feff8613b6
myarisow <mohamedyarisow22847@gmail.com>
2026-09-25 15:20:16 +0300

    submitt exercise 7

---
+++ exercise 7/main.js
@@ -0,0 +1,20 @@
+//Arrays
+
+console.log("Before")
+
+let colors = ["Red", "Blue", "Green"];
+
+console.log(colors);
+
+console.log("Afer Push");
+
+colors.push("Yellow");
+
+console.log(colors);
+
+console.log("After Shift");
+
+colors.shift();
+
+console.log(colors);
+
